164412f1efe1928e48affae95d64051407635364
Son Phan <sonvphan1@gmail.com>
2020-04-24 01:49:44 -0600

    add TableCard component and add getRestaurant fetch function

--- src/components/TableCard/TableCard.js
+++ src/components/TableCard/TableCard.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './TableCard.module.css';
 
 const TableCard = () => {
+    const [data, setData] = useState([]);
+
+    async function getRestaurants() {
+        await fetch(
+            'https://code-challenge.spectrumtoolbox.com/api/restaurants',
+            {
+                headers: {
+                    Authorization: 'Api-Key q3MNxtfep8Gt',
+                },
+            }
+        )
+            .then((res) => res.json())
+            .then(setData);
+    }
+
+    useEffect(() => {
+        getRestaurants();
+    });
+
     return (
-        <div className={classes.TableCard}>
-            <p>Name:</p>
-            <p>Genre:</p>
-            <p>Phone:</p>
-            <p>City State</p>
+        <div>
+            {data.slice(0, 10).map((item) => (
+                <div key={item.id} className={classes.TableCard}>
+                    <p>{item.name}</p>
+                    <p>{item.genre}</p>
+                    <p>{item.telephone}</p>
+                    <p>
+                        {item.city} {item.state}
+                    </p>
+                </div>
+            ))}
         </div>
     );
 };
